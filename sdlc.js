c4ada55afa1ba9493cb153a263905c834f2375e2
JBenjamin43 <jeremiahbenjamin43@gmail.com>
2022-10-21 11:50:54 -0700

    still working early summit

---
+++ sdlc.js
@@ -0,0 +1,43 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Implementation
+// Design
+// Maintain
+// Analysis of Requirements
+// Testing & Integration
+// Planning
+ 
+//                  1. Planning 
+//                  2. Analysis of Requirements
+//                  3. Design
+//                  4. Implementation
+//                  5. Testing and Intergration 
+//                  6. Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+const analysisOfRequirements= "This is the second step, this where you will break down each requirement that is needed"
+
+const Design = "this is the third step, this is were you take all of the requirements, models the way a software application will work and start to designing"
+
+const Implementation = "this is the fourth step where you will actually begin to code out your program "
+ 
+const testingAndIntergration= "this is the fifth steph where you will be testing your code checking for bugs. in a small controlled enviorment. during the intergration it will be made to more users and updated"
+
+const Maintain = "in the last phase this is where users discover bugs that were not found during testing and make sure the program is still running properly while planning for future updates"
+
+
+
+
+
